1d5cbdb1ba29fe09bdcdf9bd793d162f61ef6c2c
chen64222534 <595199567@qq.com>
2017-08-10 15:06:54 +0800

    头部样式完成，还有详细页面未做

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="header">
     <div class="content-wraper">
+
       <div class="avatar">
         <img width="64" height="64" :src="seller.avatar">
       </div>
+
       <div class="content">
         <div class="title">
           <span class="brand"></span>
@@ -19,15 +21,24 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bullentin-wraper">
-      <span class="bullentin-title"></span>
-      <span class="bullentin-text">{{seller.bulletin}}</span>
+
+    <div class="bullentin-wraper" >
+      <span class="bullentin-title"></span><span class="bullentin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
+    </div>
+
+    <div class="background">
+      <img width="100%" height="170%" :src="seller.avatar">
+    </div>
+
+    <div v-show="detailShow" class="detail">
+
     </div>
   </div>
 </template>
@@ -37,6 +48,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data () {
+      return {
+        detailShow: false
+      };
+    },
+    methods: {
+      showDetail () {
+        this.detailShow = true;
       }
     },
     created () {
