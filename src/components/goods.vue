46298873953fd381a58093adb64326307f638967
chen64222534 <595199567@qq.com>
2017-08-17 20:52:41 +0800

    goods模块上半部分完成

--- src/components/goods.vue
+++ src/components/goods.vue
@@ -9,8 +9,34 @@
       </li>
     </ul>
 
-    <div class="l-goods__food">
-      
+    <div class="l-goods__food ">
+      <ul>
+        <li class="l-goods__food-item c-food" v-for="item in goods" >
+          <h1 class="c-food__title">{{item.name}}</h1>
+          <ul>
+            <li class="c-food__list-item" v-for="food in item.foods">
+              <div class="c-food__icon">
+                <img :src="food.icon" class="c-food__icon-img">
+              </div>
+              <div class="c-food__content">
+                <h2 class="c-food__content-name">{{food.name}}</h2>
+                <div class="c-food__content-kind" v-if="food.description">{{food.description}}</div>
+                <div class="c-food__content-extra">
+                  <span class="c-food__content-sell">月售{{food.sellCount}}</span><span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="c-food__content-price">
+                  <span >￥{{food.price}}</span><span v-if="food.oldPrice" class="c-food__content-oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+              <div class="c-food__count">
+                <span class="c-food__count-subtarct"></span>
+                <span class="c-food__count-number"></span>
+                <span class="c-food__count-add"></span>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
