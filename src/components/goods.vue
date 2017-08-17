644cafa40174c1a1cf8c7ec00cba26d2ef1948eb
chen64222534 <595199567@qq.com>
2017-08-17 14:06:40 +0800

    l-goods__sidebar样式完成

--- src/components/goods.vue
+++ src/components/goods.vue
@@ -3,14 +3,14 @@
 
     <ul class="l-goods__sidebar ">
       <li class="l-goods__sidebar-item" v-for="item in goods">
-        <span class="c-sidebar__text">
+        <div class="c-sidebar__text">
           <v-icon v-show="item.type>0" :iconType="item.type" :iconSize="3" ></v-icon>{{item.name}}
-        </span>
+        </div>
       </li>
     </ul>
 
     <div class="l-goods__food">
-
+      
     </div>
   </div>
 </template>
