47603a49c42e1147bdf40f4c15596c2f34aaaaba
chen64222534 <595199567@qq.com>
2017-08-19 19:38:30 +0800

    food组件完成

--- src/components/goods.vue
+++ src/components/goods.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="l-goods">
 
-    <ul class="l-goods__sidebar ">
-      <li class="l-goods__sidebar-item" v-for="item in goods">
-        <div class="c-sidebar__text">
-          <v-icon v-show="item.type>0" :iconType="item.type" :iconSize="3" ></v-icon>{{item.name}}
-        </div>
-      </li>
-    </ul>
-
-    <div class="l-goods__food ">
+    <div class="l-goods__sidebar" ref="lGoodsSidebar">
       <ul>
-        <li class="l-goods__food-item c-food" v-for="item in goods" >
+        <li class="l-goods__sidebar-item" v-for="(item, index) in goods" :class="{'active':currentIndex===index}" @click="clickSidebar(index, $event)">
+          <div class="c-sidebar__text">
+            <v-icon v-show="item.type>0" :iconType="item.type" :iconSize="3"></v-icon>
+            {{item.name}}
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="l-goods__food" ref="lGoodsFood">
+      <ul>
+        <li class="l-goods__food-item c-food c-food-hook" v-for="item in goods" >
           <h1 class="c-food__title">{{item.name}}</h1>
           <ul>
-            <li class="c-food__list-item" v-for="food in item.foods">
+            <li class="c-food__item" v-for="food in item.foods">
               <div class="c-food__icon">
                 <img :src="food.icon" class="c-food__icon-img">
               </div>
@@ -38,11 +40,16 @@
         </li>
       </ul>
     </div>
+    <div class="l-goods__shopcart">
+      <v-shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></v-shopcart>
+    </div>
   </div>
 </template>
 
 <script>
+  import shopcart from './shopcart.vue';
   import textIcon from '../common/textIcon.vue';
+  import BScroll from 'better-scroll';
 
   const ERR_OK = 0;
 
@@ -57,22 +64,76 @@
     },
     data () {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       };
+    },
+    computed: {
+//      获取当前高度对应区间
+      currentIndex () {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i;
+          }
+        }
+        return 0;
+      }
     },
     created () {
       this.$http.get('/api/goods').then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
-//          console.log(response);
+          this.$nextTick(() => {
+            this._initScroll();
+            this._calculateHeight();
+          });
         }
       }, (response) => {
       }
      );
     },
     components: {
-      'v-icon': textIcon
+      'v-icon': textIcon,
+      'v-shopcart': shopcart
+    },
+    methods: {
+//      better-scroll绑定模块，及监听scroll事件
+      _initScroll () {
+        this.foodScroll = new BScroll(this.$refs.lGoodsFood, {
+          probeType: 3
+        });
+        this.sidebarScroll = new BScroll(this.$refs.lGoodsSidebar, {
+          click: true
+        });
+
+        this.foodScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+//      创建food每个组对应高度的数组
+      _calculateHeight () {
+        let foodList = this.$refs.lGoodsFood.getElementsByClassName('c-food-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      },
+//      点击sidebar,滚动food
+      clickSidebar (index, event) {
+        if (!event._constructed) {
+          return;
+        }
+        let foodList = this.$refs.lGoodsFood.getElementsByClassName('c-food-hook');
+        let el = foodList[index];
+        this.foodScroll.scrollToElement(el, 1000);
+      }
     }
   };
 </script>
---
+++ src/components/shopcart.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="c-shopcart">
+    <div class="c-shopcart__leftContent">
+      <div class="c-shopcart__logo">
+        <div class="c-shopcart__logo-icon">
+          <span class="icon-shopping_cart"></span>
+        </div>
+        <div class="c-shopcart__count">{{totalCount}}</div>
+      </div>
+      <div class="c-shopcart__total">
+        <span class="c-shopcart__price">￥{{totalPrice}}</span>
+        <span class="c-shopcart__extra">另需配送费￥{{deliveryPrice}}元</span>
+      </div>
+
+    </div>
+    <div class="c-shopcart__rightContent">
+      <div class="c-shopcart__explain">
+        ￥{{minPrice}}起送
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    props: {
+      selectFoods: {
+        type: Array,
+        default () {
+          return [{price: 10,
+            count: 4}, {price: 20,
+              count: 1}];
+        }
+      },
+      deliveryPrice: {
+        type: Number,
+        default: 0
+      },
+      minPrice: {
+        type: Number,
+        default: 0
+      }
+    },
+    computed: {
+      totalPrice () {
+        let price = 0;
+        this.selectFoods.forEach((food) => {
+          price += (food.price * food.count);
+        });
+        return price;
+      },
+      totalCount () {
+        let count = 0;
+        this.selectFoods.forEach((food) => {
+          count += food.count;
+        });
+        return count;
+      }
+    }
+  };
+</script>
